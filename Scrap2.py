22b59b7e693481960fafca3f8d5ad275c15999f8
Emyra <sofienedami@gmail.com>
2020-03-12 00:40:52 +0100

    First Test instagram Bot

---
+++ Scrap2.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+instagram_account = 'sintegraconsulting'
+
+story_saver = "https://www.storysaver.net"
+
+driver.get(story_saver)
+account_input = driver.find_element_by_name('text_username').send_keys(instagram_account)
+
+time.sleep(2)
+search_profile_button = driver.find_element_by_id('StoryButton')
+search_profile_button.click()
+time.sleep(5)
+while True:
+    try:
+        save_video = driver.find_element_by_link_text("Save as Video")
+       # save_video = driver.find_elements_by_class_name("button medium primary")
+        save_video.click()
+        time.sleep(1)
+    except:
+        pass
+        print("trying to scroll")
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Scraping.py
@@ -0,0 +1,6 @@
+"""i = 1
+while i < 10:
+    driver.execute_script("window.scrollTo(0, 3000)")
+    search_profile_button = driver.find_element_by_id('StoryButton')
+    search_profile_button.click()
+    i += 1"""
